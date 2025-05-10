33d637cb1330b79e09328cd48ae817b36241cde0
Gump <2382175384@qq.com>
2025-05-10 12:44:04 +0800

    Format maxDepth.cpp using clang-format

--- 5.10/maxDepth.cpp
+++ 5.10/maxDepth.cpp
@@ -1,20 +1,24 @@
 // Link:
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 struct TreeNode {
-int val;
-TreeNode *left;
-TreeNode *right;
-TreeNode() : val(0), left(nullptr), right(nullptr) {}
-TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
 };
 class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        return root == nullptr? 0 : max(maxDepth(root->left), maxDepth(root->right)) +1;
+   public:
+    int maxDepth(TreeNode *root) {
+        //
+        return root == nullptr
+                   ? 0
+                   : max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
